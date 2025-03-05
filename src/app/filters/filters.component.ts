1978e97f476c6335a5717de505be25efca8296be
KANTNOLI <zahargysev02@gmail.com>
2025-03-05 14:39:33 +0300

    Creating bts and input

--- src/app/filters/filters.component.ts
+++ src/app/filters/filters.component.ts
@@ -11,6 +11,34 @@ import { Carts } from '../carts/carts.component';
 export class FiltersComponent {
   carts: Cart[] = Carts;
   search: string = ""
+  historyClick: number = -1;
+
+  setSort(id: number) {
+    if (id === this.historyClick) {
+      this.carts.reverse()
+    } else {
+      switch (id) {
+        case 1:
+          this.carts.sort((a, b) => a.price - b.price);
+          break;
+        default: this.carts.sort()
+      }
+    }
+
+
+    this.historyClick = id
+
+  }
+
+  setCarts() {
+    console.log(1);
+
+    this.carts = Carts.filter((cart) => cart.title.toLocaleLowerCase().includes(this.search.toLocaleLowerCase()))
+
+    
+    console.log(this.carts);
+  }
+
 }
 
 
--- src/assets/RenderArrayCarts.module.ts
+++ src/assets/RenderArrayCarts.module.ts
@@ -10,7 +10,7 @@ export const RenderCarts = (length: number): Cart[] => {
     return Array.from({ length: length }, () => ({
         img: "https://i.pinimg.com/originals/fc/06/61/fc0661102c02495be896a4fdef51c94e.jpg",
         price: rand(10000,100000),
-        title: `Name ${temp[rand(0, temp.length)]}`,
-        desc: `Desc ${temp[rand(0, temp.length)]} `.repeat(rand(0, 10)),
+        title: `Name ${temp[rand(0, temp.length - 1)]}`,
+        desc: `Desc ${temp[rand(0, temp.length - 1)]} `.repeat(rand(0, 10)),
     }))
 }
