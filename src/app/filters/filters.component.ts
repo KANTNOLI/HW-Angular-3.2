e30ee17e14131ab9af5839b3a38564f90f7f5ef6
KANTNOLI <zahargysev02@gmail.com>
2025-03-05 14:30:04 +0300

    Add forms module and starting using

--- src/app/filters/filters.component.ts
+++ src/app/filters/filters.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Cart } from '../../assets/Interfaces/Carts.interface';
 import { Carts } from '../carts/carts.component';
 
 @Component({
@@ -8,7 +9,8 @@ import { Carts } from '../carts/carts.component';
   styleUrl: './filters.component.css'
 })
 export class FiltersComponent {
-
+  carts: Cart[] = Carts;
+  search: string = ""
 }
 
 
